45c14386430d8fdc6f150c8bf36545a285981d73
Kai-Arne Reiter <git@mirrorspock.com>
2017-07-04 09:47:48 +0200

    updating to use seacon calendar

--- jobs/calendar.rb
+++ jobs/calendar.rb
@@ -8,7 +8,7 @@ require 'net/http'
 def get_todays_events_from_calendar url
   calendar = Icalendar.parse(open(url))
   events = calendar.first.events.sort { |a,b| b.dtstart <=> a.dtstart }
-
+  byebug
   events.each_with_object([]) do |event, summaries|
     if DateTime.now.between?(Date.parse(event.dtstart.to_s), Date.parse(event.dtend.to_s))
       summaries.push(event.summary)
@@ -22,7 +22,7 @@ end
 
 SCHEDULER.every '10s' do
     # TODO: read from config
-    calendar_url = "https://calendar.google.com/calendar/ical/kabisa.nl_eohmu33rq098a6581fj3qvbuko%40group.calendar.google.com/public/basic.ics"
+    calendar_url = "https://calendar.google.com/calendar/ical/seacon.mobi_hbr6hlsvua0btlbhjb0ji98gvc%40group.calendar.google.com/public/basic.ics"
 
     events = get_todays_events_from_calendar calendar_url
     text = events_to_text events
